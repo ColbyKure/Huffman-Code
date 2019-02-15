a2c387bcdaea075f18a488b02d93073f7ef9a651
Sunny Sun <sunnysun1997@gmail.com>
2019-02-14 16:00:13 -0800

    edited bitwise compress

--- src/compress.cpp
+++ src/compress.cpp
@@ -106,6 +106,87 @@ void compressBitwise(const string & infile, const string & outfile) {
     // TODO (final)
     cerr << "TODO: compress '" << infile << "' -> '"
         << outfile << "' here (bitwise)" << endl;
+    
+    ifstream in;
+    const char * input = infile.c_str();
+    in.open(input, ios::binary);
+
+    //check if file actually opened 
+    if(!in.is_open()) {
+        cout << "input file was not opened...\n";
+        return;
+    }
+
+    //check for empty file
+    in.seekg(0, ios_base::end);
+    unsigned int len = in.tellg();
+    if(len == 0) {
+        cout << "input file opened but empty\n";
+        return;
+    }
+
+    //find beginning of stream
+    in.seekg(0, ios_base::beg);
+
+    //read lines from stream
+    vector<int> freqs(256, 0); 
+    unsigned char nextChar;
+    int nextByte, index;
+    while((nextByte = in.get()) != EOF) {
+        nextChar = (unsigned char) nextByte;
+        index = (int)nextChar;
+        freqs[index] = freqs[index] + 1;
+    }
+
+    //build tree
+    HCTree tree;
+    tree.build(freqs);
+    
+    //open out stream
+    const char * output = outfile.c_str();
+    ostream outS(output);
+    BitOutputStream out(outS);
+    
+    //output header
+    unsigned int currentFreqs;
+    unsigned int mask = 0x1;
+    mask = mask << 31;
+    for(int i = 0; i < 256; ++i) {
+	currentFreqs = freqs[i]; 
+	for (int k = 0; k < 32, k++){
+            currentFreqs = currentFreqs & mask;
+	    if (currentFreqs != 0){ //if the bit is one
+	    	out.writeBit(1);
+	    }
+	    else{
+	    	out.writeBit(0);
+	    } 
+	}
+    }
+
+    if(in.is_open()) {
+        in.close();
+    }
+
+    in.open(input, ios::binary); //reopen input
+
+    in.seekg(0, ios_base::beg);
+
+    while((nextByte = in.get()) != EOF) { //read infile again
+        nextChar = (unsigned char) nextByte;
+        tree.encode(nextChar, out);
+    }
+    
+    
+    
+    //close files
+   if(outS.is_open()) {
+
+        outS.close();
+    }
+    if(in.is_open()) {
+        in.close();
+    }
 }
 
 int main(int argc, char ** argv) {
